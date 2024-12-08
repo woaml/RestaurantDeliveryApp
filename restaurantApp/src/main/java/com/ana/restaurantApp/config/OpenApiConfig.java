def2f49b4f1f49fdd4dfdc40ce2aee7834da358e
woaml <set8el1@gmail.com>
2024-12-08 19:00:39 +0300

    added open api

---
+++ restaurantApp/src/main/java/com/ana/restaurantApp/config/OpenApiConfig.java
@@ -0,0 +1,50 @@
+package com.ana.restaurantApp.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Anastasia",
+                        email = "set8el1@gmail.com"
+                ),
+                description = "OpenApi documentation",
+                title = "OpenApi specification",
+                version = "1.0",
+                license = @License(
+                        name = "name"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:5454"
+                ),
+                @Server(
+                        description = "Prod"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
